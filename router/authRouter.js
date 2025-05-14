8471057b518cf696727aa0f8518bcc8909215a66
Sebastian Buritica <jhoseburitica40@gmail.com>
2025-05-14 16:13:46 -0500

    Login, register y logout de un user, listos

--- router/authRouter.js
+++ router/authRouter.js
@@ -2,22 +2,27 @@
 import { Router } from "express";
 import validator from "../Middlewares/validator.js"
 
-//Necesario para Register
+//Necesario para Register/SignUp
 import register from "../controllers/Auth/singup.js";
 import schemaRegister from "../Schemas/auth/register.js"
 import createHash from "../Middlewares/Auth/Register/createHash.js"
 import accountExist from "../Middlewares/Auth/Register/accountexists.js"
 
-//Necesario para Login
+//Necesario para Login/SignIn
 import signin from "../controllers/Auth/signin.js";
 import schemaLogin from "../Schemas/auth/singin.js"
 import accountNotExist from "../Middlewares/Auth/Login/accountNotExist.js";
 import validatedPassword from "../Middlewares/Auth/Login/validatedPassword.js";
 import generateToken from "../Middlewares/Auth/Login/generateToken.js";
 
+//Necesario para SignOut/LogOut
+import singout from "../controllers/Auth/singout.js";
+
 const routerAuth = Router();
 
 routerAuth.post("/signin",validator(schemaLogin),accountNotExist,validatedPassword,generateToken,signin)
+
+routerAuth.post("/signout",accountNotExist, singout)
 
 routerAuth.post("/register", validator(schemaRegister),accountExist,createHash, register)
 
