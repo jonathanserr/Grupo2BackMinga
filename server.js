aeea3fcad7b7163ed32cb13f8f745e64f184e7e8
Sebastian Buritica <jhoseburitica40@gmail.com>
2025-05-15 10:37:22 -0500

    Resolucion de conflictos

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ import './config/database.js'
 import cors from 'cors';
 import morgan from 'morgan';
 import routerIndex from './router/index.js';
-<<<<<<< HEAD
-import error_404 from './middlewares/error_404.js';
-import error_handler from './middlewares/error_handler.js';
-=======
 import error400 from './Middlewares/error400.js';
 import error404 from './Middlewares/error404.js';
 import error500 from './Middlewares/error500.js';
 
->>>>>>> develop
 
 const server = express(); 
 
@@ -28,17 +23,11 @@ server.use(morgan('dev'));
 
 //  configure routes:
 server.use('/api',routerIndex);
-<<<<<<< HEAD
-//server.use(not_found_handler);
- server.use(error_404);
-server.use(error_handler);
-=======
 
 
 server.use(error404)
 server.use(error400)
 server.use(error500)
 
->>>>>>> develop
 
 server.listen(PORT, ready);
