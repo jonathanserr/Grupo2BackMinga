2b4d90676036434fe4c51269c6a24cac884720b1
Sebastian Buritica <jhoseburitica40@gmail.com>
2025-05-16 17:55:45 -0500

    Se implementó la ruta de obtener comentarios

--- controllers/Comment/read.js
+++ controllers/Comment/read.js
@@ -1,9 +1,15 @@
+import { response } from "express"
+import Comments from "../../models/Comment.js"
+import Chapter from "../../models/Chapter.js"
+import Author from "../../models/Author.js"
+
 const getComments = async (req,res,next)=>{
     try {
-        
-        let respuesta = "getComments "
-        //Maqueta para luego implementar realmente
-        res.send(respuesta)
+        const getComments = await Comments.find().populate("chapter_id").populate("author_id")
+        return res.status(200).json({
+            succes:true,
+            response: getComments
+        })
     } catch (error) {
         next(error)
     }
--- controllers/Manga/read.js
+++ controllers/Manga/read.js
@@ -4,7 +4,7 @@ import Category from "../../models/Category.js"
 const getManga = async (req,res,next)=>{
     try {
     
-    const getMangas = await Manga.find().populate("category_id")
+    const getMangas = await Manga.find().populate("category_id").populate("author_id")
 
     return res.status(200).json({
         succes: true, 
